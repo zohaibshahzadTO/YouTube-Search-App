0f6216ae90bbb6004a2ab28a296a90ab93c0abb6
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-03-18 19:25:13 -0400

    Fixed typos + Added a check/assertion
    - Theres a bit of load time that occurs for the videos and at the same time, React is constantly rendering all components which causes an error and UI flaw. We added a check (if statement) saying if there is no video yet, then state that its still loading, if not, the video and components will load regardless.

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const VideoDetail = ({video}) => {
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+
   const videoId = video.id.videoId;
   const url = `https://www.youtube.com/embed/${videoId}`;
 
@@ -12,6 +16,9 @@ const VideoDetail = ({video}) => {
       <div className="details">
         <div>{video.snippet.title}</div> // title
         <div>{video.snippet.description}</div> // description
+      </div>
     </div>
   );
 };
+
+export default VideoDetail;
