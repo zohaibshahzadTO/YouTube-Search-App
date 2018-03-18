3025e3515704a7c89a78c973007b9416933e00e5
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-03-18 14:05:24 -0400

    Worked on Video_list.js
    - Since this file wont be recording any states, we can just use a functional component. Created VideoList function and used bootstrap "col-md-4" to list a column of videos (4) and exported to index.js and inserted the import as well.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyB2VIfUHF6jQ0MMONwBE_rmURoHqvsUA8A';
 
 class App extends Component {
@@ -20,6 +21,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList />
       </div>
     );
   }
