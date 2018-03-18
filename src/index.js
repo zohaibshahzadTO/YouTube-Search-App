cf658f99f8bef7e547a449c8c2f2a52a23b888d0
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-03-18 13:48:11 -0400

    Converted App function into class based component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyB2VIfUHF6jQ0MMONwBE_rmURoHqvsUA8A';
 
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
   console.log(data);
 });
 
-// Create a new component. This component should produce some HTML.
-// After we need to make sure it goes to the DOM.
-const App = () => {
-  return <div><SearchBar /></div>;
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
-// Take this components generated HTML and put it on the DOM (or page).
+
 ReactDOM.render(<App />, document.querySelector('.container'));
