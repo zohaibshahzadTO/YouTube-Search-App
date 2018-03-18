70ee136431ae5befbc31a91f0745f7e919b34097
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-03-18 19:51:24 -0400

    Implementing Callback
    - We implemented a callback from app to video list to videolistitem for the selectedVideo function

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 // Intended to show a single video (thumbnail, list item).
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   const imageUrl = video.snippet.thumbnails.default.url;
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageUrl} />
--- src/index.js
+++ src/index.js
@@ -17,7 +17,10 @@ class App extends Component {
     };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
       // this.setState({ videos: videos })
     });
   }
@@ -27,7 +30,9 @@ class App extends Component {
       <div>
         <SearchBar />
         <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
 
       </div>
     );
