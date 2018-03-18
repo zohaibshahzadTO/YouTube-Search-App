f2eeb6d4111e18cc1530f6d548ac119f06986b51
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-03-18 17:43:25 -0400

    Video List Item.js
    Worked on video_list_item.js file. Created a functional componenent intended to show an individual video which we then pass to the video_list.js component file.

---
+++ src/components/video_list_item.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+// Intended to show a single video (thumbnail, list item).
+const VideoListItem = (props) => {
+  return <li>Video</li>
+}
+
+export default VideoListItem;
