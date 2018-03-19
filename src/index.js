20027b3ea03fc6832de056660461a94f8f0226d6
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-03-18 20:22:31 -0400

    Throttled Search Ability
    - Installed lodash package into project to throttle search function.
    - We used debounce to run the searchBar to trigger every 300 ms.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+
 
 const API_KEY = 'AIzaSyB2VIfUHF6jQ0MMONwBE_rmURoHqvsUA8A';
 
@@ -29,9 +31,11 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
+
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
