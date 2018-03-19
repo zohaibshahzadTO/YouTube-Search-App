571e4baac55cdc7656704329cd05b4e985986202
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-03-18 20:10:09 -0400

    Ability to search for other videos
    - Created a new method/function called videoSearch(term) and copy pasted the Youtube Search Code snippet into it (DRY principle) and made the callback to search_bar.js and rendered it

--- src/index.js
+++ src/index.js
@@ -16,19 +16,22 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards'); // initial search
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: 'term'}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       });
-      // this.setState({ videos: videos })
     });
   }
 
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
